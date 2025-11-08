b18dfb1ccdc152f4b7a75c13787a674eb6e09e48
Polina Grabar <polina.grabar@gmail.com>
2025-11-08 23:46:52 +0100

    initial commit

---
+++ src/main/java/com/pg/leverx/hw3/StoreThreads.java
@@ -0,0 +1,11 @@
+package com.pg.leverx.hw3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreThreads {
+    public static void main(String[] args) {
+        SpringApplication.run(StoreThreads.class, args);
+    }
+}
